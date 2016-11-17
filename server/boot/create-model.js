213e576ca9d63940dff4fd8011203aa3546e22a0
PereMekna <dupont.louis4@gmail.com>
2016-11-17 08:51:01 +0100

    Add control model

--- server/boot/create-model.js
+++ server/boot/create-model.js
@@ -1,18 +1,6 @@
 /*module.exports = function(app) {
-  app.dataSources.postgres.automigrate('setting', function(err) {
+  app.dataSources.postgres.automigrate('control', function(err) {
     if (err) throw err;
-      console.log('Models created: \n', setting);
-    });
-  app.dataSources.postgres.automigrate('temperature', function(err) {
-    if (err) throw err;
-      console.log('Models created: \n', temperature);
-    });
-  app.dataSources.postgres.automigrate('alert', function(err) {
-    if (err) throw err;
-      console.log('Models created: \n', alert);
-    });
-  app.dataSources.postgres.automigrate('detection', function(err) {
-    if (err) throw err;
-      console.log('Models created: \n', detection);
+      console.log('Models created: \n', control);
     });
 };*/
